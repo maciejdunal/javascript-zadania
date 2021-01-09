c0779863ff9abeb3d86625e0b4f9cb73a7dd53b0
maciejdunal <72334603+maciejdunal@users.noreply.github.com>
2021-01-10 00:06:35 +0100

    Add files via upload

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,19 @@
+//Zadanie 1:
+
+console.log("Maciej");
+
+//Zadanie 2:
+
+console.log(12+32);
+console.log(52/16);
+console.log(5*2);
+
+// Zadanie 3:
+
+console.log("Hello World");
+//console.log(Hello World);
+//console.log("Hello World);
+            
+//W punkcie 1 pojawia się string
+//W punkcie 2 pojawia się błąd bo nie ma takiej zmiennej
+//W punkcie 3 pojawia się błąd bo brakuje cudzysłowa
