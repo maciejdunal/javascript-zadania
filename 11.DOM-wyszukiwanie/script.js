7cea546bb06991d06e97f7e35433affd3a8d9388
maciejdunal <72334603+maciejdunal@users.noreply.github.com>
2021-01-10 12:33:17 +0100

    Add files via upload

---
+++ 11.DOM-wyszukiwanie/script.js
@@ -0,0 +1,35 @@
+//Exercise 1
+
+const list = document.getElementsByClassName("list");
+console.log(list);
+
+//Exercise 2
+
+function GetElements(tag) {
+    return document.getElementsByTagName(tag);
+}
+console.log(GetElements("ul"));
+
+//Exercise 3
+
+const x = document.getElementById("list");
+console.log(x);
+
+//Exercise 4
+
+const showdown = (lis, uls, spans, sid, sids) => {
+    const allLis = document.querySelectorAll(lis);
+const allUls = document.querySelectorAll(uls);
+const allSpans = document.querySelectorAll(spans);
+
+const spanInDiv = document.querySelectorAll(sid);
+const spanInDivWID = document.querySelectorAll(sids);
+
+allLis.forEach(al => {console.log(al)});
+allUls.forEach(au => {console.log(au)});
+allSpans.forEach(as => {console.log(as)});
+
+spanInDiv.forEach(sid => {console.log(sid)});
+spanInDivWID.forEach(sidwid => {console.log(sidwid)});
+};
+showdown("li", "ul", "span", "div.list span", "div#spans span");
